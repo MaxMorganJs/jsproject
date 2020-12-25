5d03f18e13cf55e262c4cb6a36eae622b2d24102
Morgan <yarvice@gmail.com>
2020-12-25 10:20:40 +0300

    first commit

---
+++ src/js/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let number = 323;
+
+const person = "5";
+
+const bool = true;
+
+//console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name, obj.age, obj.isMarried);
+//console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
